6f1d04b3e77cfa8d0b69b2f0ce1ee7c57c3c812c
TheMaxium69 <mxm.tournier@gmail.com>
2025-03-05 00:32:22 +0100

    Update Other.php

--- core/Controllers/Other.php
+++ core/Controllers/Other.php
@@ -1,1 +1,83 @@
 <?php
+
+namespace Controllers;
+
+class Other extends Controller
+{
+    protected $modelDefault = \Model\Users::class;
+    protected $modelTyroServUser = \Model\UsersTyroServ::class;
+    protected $modelTyroServToken = \Model\TokenTyroServ::class;
+
+    /**
+     *
+     * Wiew index
+     * @method : get
+     *
+     */
+    public function index()
+    {
+
+        $message = "Bienvenue dans l'API Useritium pour Autre";
+
+        header('Access-Control-Allow-Origin: *');
+        echo json_encode($message);
+    }
+
+    /**
+     *
+     * Connect Other API
+     * @method : post
+     *
+     */
+    public function connect()
+    {
+
+        if (!empty($_POST['email_useritium']) && !empty($_POST['mdp_useritium'])) {
+
+            $email_auth = $_POST['email_useritium'];
+            $mdp_auth = $_POST['mdp_useritium'];
+
+            $userLoad = $this->default->findByEmail($email_auth);
+
+            if (!empty($userLoad)) {
+
+                $mdpCrypt_auth = $this->default->chiffreMdp($mdp_auth);
+
+                if ($mdpCrypt_auth == $userLoad->password) {
+
+                    // GOOD
+
+                    $resultCompteUseritium = ["id" => $userLoad->id,
+                        "email"=> $userLoad->email,
+                        "username"=> $userLoad->username,
+                        "displayName"=> $userLoad->displayname,
+                        "pp"=>$userLoad->pp,
+                    ];
+
+                    header('Access-Control-Allow-Origin: *');
+                    echo json_encode(["status"=>"true","why"=>"successfully connected","result"=>$resultCompteUseritium]);
+
+                } else {
+
+                    header('Access-Control-Allow-Origin: *');
+                    echo json_encode(["status"=>"err","why"=>"bad passwd"]);
+
+
+                }
+
+            } else {
+
+                header('Access-Control-Allow-Origin: *');
+                echo json_encode(["status"=>"err","why"=>"bad email"]);
+
+            }
+
+        } else {
+
+            header('Access-Control-Allow-Origin: *');
+            echo json_encode(["status"=>"err","why"=>"wrong"]);
+
+        }
+    }
+
+}
